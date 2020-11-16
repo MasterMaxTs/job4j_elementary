71029992af1dfba5ed4654187d376d319517a0f1
MasterMaxTs <max86ts@gmail.com>
2020-11-16 21:30:35 +0300

    9.1. String [#407301]. Сhecked with CheckStyle

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -1,24 +1,24 @@
 package ru.job4j.calculator;
 
 public class ArgMethod {
-    public static void hello (String name) {
-        System.out.println("Hello, "+name);
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
     }
 
-    public static void hello (){
+    public static void hello() {
         System.out.println("Hello!");
     }
 
-    public static void hello (String name, int age){
-        System.out.println("Hello, "+name + ". Your age is "+age);
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ". Your age is " + age);
     }
 
-    public static void hello (int age){
-        System.out.println("Hello, your age is "+age);
+    public static void hello(int age) {
+        System.out.println("Hello, your age is " + age);
     }
 
-    public static void hello (String name1, String name2, String name3){
-        System.out.println("Hello, "+name1+".Hello, "+name2+".Hello "+name3);
+    public static void hello(String name1, String name2, String name3) {
+        System.out.println("Hello, " + name1 + ".Hello, " + name2 + ".Hello " + name3);
     }
 
     public static void main(String[] args) {
