f4190bb26d632327765019684f88b27f2f853dff
MasterMaxTs <max86ts@gmail.com>
2020-11-16 22:03:18 +0300

    8. Стороны прямоугольника. [#407294]. Расчет через три формулы. Сhecked with CheckStyle

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,21 +1,20 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square (int p, int k) {
-        double h = p/(2*(k+1));
-        double l = h*k;
-        double s = h*l;
-        return s;
+    public static double square(int p, int k) {
+        double h = (double) p / (2 * (k + 1));
+        double l = h * k;
+        return h * l;
         /*double rsl = 0.25*k*Math.pow(p/(k+1), 2);
         return rsl;*/
 
     }
 
     public static void main(String[] args) {
-        double result = SqArea.square(4,1);
+        double result = SqArea.square(4, 1);
         System.out.println("p = 4, k = 1, s = 1, real = " + result);
 
-        result = SqArea.square(6,2);
+        result = SqArea.square(6, 2);
         System.out.println("p = 6, k = 2, s = 2, real = " + result);
     }
 }
