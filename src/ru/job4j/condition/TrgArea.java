fbad20155969e71945186279b2f246bc6e3b0436
MasterMaxTs <max86ts@gmail.com>
2020-11-16 22:06:11 +0300

    9. Площадь треугольника. [#407297]. Сhecked with CheckStyle

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -1,14 +1,13 @@
 package ru.job4j.condition;
 
 public class TrgArea {
-    public static double area (double a, double b, double c) {
-        double p05 = 0.5*(a+b+c);
-        double rsl = Math.sqrt(p05*(p05-a)*(p05-b)*(p05-c));
-        return rsl;
+    public static double area(double a, double b, double c) {
+        double p05 = 0.5 * (a + b + c);
+        return Math.sqrt(p05 * (p05 - a) * (p05 - b) * (p05 - c));
     }
 
     public static void main(String[] args) {
-        double rsl = TrgArea.area(2,2,2);
+        double rsl = TrgArea.area(2, 2, 2);
         System.out.println("area (2, 2, 2) = " + rsl);
     }
 }
