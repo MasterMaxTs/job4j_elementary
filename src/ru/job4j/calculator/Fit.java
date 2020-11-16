dd4a4de04d00b6ffad56400c77e9939da54b2a16
MasterMaxTs <max86ts@gmail.com>
2020-11-16 21:48:03 +0300

    6. Идеальный вес [#407291]. Сhecked with CheckStyle

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -1,22 +1,20 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight (short height){
-        double rsl = (height - 100)*1.15;
-        return rsl;
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
     }
 
-    public static double womanWeight (short height){
-        double rsl = (height - 110)*1.15;
-        return rsl;
+    public static double womanWeight(short height) {
+        return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
         short height = 182;
         double man = Fit.manWeight(height);
-        System.out.println("Man "+height+" cm" +" is "+man+" kg.");
+        System.out.println("Man " + height + " cm" + " is " + man + " kg.");
 
         double woman = Fit.womanWeight(height);
-        System.out.println("Woman "+height+" cm" +" is "+woman+" kg.");
+        System.out.println("Woman " + height + " cm" + " is " + woman + " kg.");
     }
 }
