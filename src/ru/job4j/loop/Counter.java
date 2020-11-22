8f916342e4bd31dbdf23c5daa31b2249cbe1d2a5
MasterMaxTs <max86ts@gmail.com>
2020-11-22 12:36:26 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#407356]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -9,14 +9,30 @@ public class Counter {
         return sum;
     }
 
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println("Сумма чисел из диапазона (0;10) равна "
                 + Counter.sum(0, 10));
+        System.out.println("Сумма чётных чисел из диапазона (0;10) равна "
+                + Counter.sumByEven(0, 10));
 
         System.out.println("Сумма чисел из диапазона (3;8) равна "
                 + Counter.sum(3, 8));
+        System.out.println("Сумма чётных чисел из диапазона (3;8) равна "
+                + Counter.sumByEven(3, 8));
 
         System.out.println("Сумма чисел из диапазона (1;1) равна "
                 + Counter.sum(1, 1));
+        System.out.println("Сумма чётных чисел из диапазона (1;1) равна "
+                + Counter.sumByEven(1, 1));
     }
 }
--- test/ru/job4j/loop/CounterTest.java
+++ test/ru/job4j/loop/CounterTest.java
@@ -9,10 +9,12 @@ public class CounterTest {
     @Test
     public void whenFrom10To20sum165() {
         assertThat(Counter.sum(10, 20), is(165));
+        assertThat(Counter.sumByEven(10, 20), is(90));
     }
 
     @Test
     public void whenFromMin1To6sum20() {
         assertThat(Counter.sum(-1, 6), is(20));
+        assertThat(Counter.sumByEven(-1, 6), is(12));
     }
 }
