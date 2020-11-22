aaf4d04ce735da380e82b917be821b0b3c65171e
MasterMaxTs <max86ts@gmail.com>
2020-11-22 14:54:24 +0300

    5.4. Протеиновая диета [#407348

---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class FitnessTest {
+
+    @Test
+    public void whenIvanGreatNik() {
+        assertThat(Fitness.calc(95, 90), is(0));
+    }
+
+    @Test
+    public void whenIvanLessNik() {
+        assertThat(Fitness.calc(80, 100), is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        assertThat(Fitness.calc(50, 90), is(2));
+    }
+}
