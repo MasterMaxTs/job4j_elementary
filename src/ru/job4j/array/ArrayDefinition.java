55b59e741eb0e51afb520c732fd518610c2ae1f1
MasterMaxTs <max86ts@gmail.com>
2020-11-24 23:50:44 +0300

    6.0.2. Заполнение массива. [#407272]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,17 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Maxim";
+        names[1] = "Sergey";
+        names[2] = "Sasha";
+        names[3] = "Pasha";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
