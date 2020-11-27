ff07889d8af2a6f8382a2d188eddc5f9c2c41dab
MasterMaxTs <max86ts@gmail.com>
2020-11-27 18:58:04 +0300

    6.5.3. Сортировка выборкой. [#407266]

---
+++ src/ru/job4j/array/SortSelected.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+public class SortSelected {
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+
+            int min = Min.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexOf(data, min);
+
+            SwitchArray.swap(data, index, i);
+        }
+        return data;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 6, 4, 1, 7, 5, 2};
+        for (int arr : array) {
+            System.out.print(arr);                // Not sorted
+        }
+        System.out.println();
+
+        for (int el : sort(array)) {              // Sort by selected
+            System.out.print(el);
+        }
+    }
+}
---
+++ test/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SortSelectedTest {
+
+    @Test
+    public void whenSort() {
+        assertThat(SortSelected.sort(new int[] {2, 7, 5, 9, 1}),
+                is(new int[] {1, 2, 5, 7, 9}));
+    }
+}
