5df2e81f6fd09ae4229ba73c2c2048a6ee6e39f4
MasterMaxTs <max86ts@gmail.com>
2020-12-13 15:04:38 +0300

    2. Scanner и чтение числа из консоли. [#407401]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
+    private static final int[] ARRAY = {1, 2, 3};
     private static int value = 11;
     private static int count = 1;
 
@@ -10,14 +11,27 @@ public class Matches {
         return count % 2 == 0 ? 2 : 1;
     }
 
+    public static boolean validate(int select) {
+        boolean rsl = false;
+        for (int i = 0; i < ARRAY.length; i++) {
+            if (ARRAY[i] == select) {
+                return true;
+            }
+        }
+        return rsl;
+    }
+
+
     public static void selecting() {
         while (value > 3) {
             System.out.println("Player #" + getPlayer(count) + " is choosing from 1 to 3 match: ");
             Scanner scanner = new Scanner(System.in);
             int select = Integer.parseInt(scanner.nextLine());
-            value -= select;
-            System.out.println("There were " + value +  " matches left on the table");
-            count++;
+            if (validate(select)) {
+                value -= select;
+                System.out.println("There were " + value + " matches left on the table");
+                count++;
+            }
         }
     }
 
