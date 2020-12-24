3e8614b02f277a1b543843b52eec42edf9f305dc
MasterMaxTs <max86ts@gmail.com>
2020-12-24 21:09:01 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#407420]

--- src/ru/job4j/ex/Fact.java
+++ src/ru/job4j/ex/Fact.java
@@ -8,12 +8,12 @@ public class Fact {
     }
 
     public int calc(int n) {
+        if(n < 0) {
+            throw new IllegalArgumentException("factorial of negative number does not exist!");
+        }
         int rsl = 1;
         for (int i = 1; i <= n; i++) {
             rsl *= i;
-        }
-        if(n < 0) {
-            throw new IllegalArgumentException("factorial of negative number does not exist!");
         }
         return rsl;
     }
