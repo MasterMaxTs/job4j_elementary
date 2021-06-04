92735d3a9076f4240c1e6265f289769c6183493f
Maxim Tsurkanov <max86ts@gmail.com>
2021-06-04 13:02:12 +0300

    6.6.8. Двухмерный массив. Таблица умножения. [#407258]. Тип возвращаемого метода изменен на String

--- src/ru/job4j/array/Matrix.java
+++ src/ru/job4j/array/Matrix.java
@@ -11,14 +11,20 @@ public class Matrix {
         return table;
     }
 
-    public static void main(String[] args) {
-        int size = 9;
-        int[][]table = multiple(size);
+    public static String showTable(int size) {
+        int[][] table = multiple(size);
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < table.length; i++) {
             for (int j = 0; j < table.length; j++) {
-                System.out.print(" " + table[i][j]);
+                stringBuilder.append("  ");
+                stringBuilder.append(table[i][j]);
             }
-            System.out.println();
+            stringBuilder.append("\n");
         }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Matrix.showTable(9));
     }
 }
